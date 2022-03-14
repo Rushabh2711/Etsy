e818882f032dcd5eed86ab20cce27d1bb7d5a152
Rushabh2711 <shetarushabh@gmail.com>
2022-03-13 19:55:15 -0700

    order api created

--- backend/src/routes/order.js
+++ backend/src/routes/order.js
@@ -1,7 +1,26 @@
 const express = require('express');
-const User = require('../service/User');
+const Product = require('../service/Product');
+const Order = require('../service/Order');
 const router = new express.Router();
 
+router.post('/createOrder', async (req, res) => {
+    try {
+        await Order.createOrder(req.body.order);
+        res.status(200).send();
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
+    }
+});
 
+router.post('/getOrder', async (req, res) => {
+    try {
+        const orders = await Order.getOrder(req.body.user_id);
+        res.status(200).send(orders);
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
+    }
+});
 
 module.exports = router;
