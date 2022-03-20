722adf2cead2d744c948211d9279e07e7524b92f
Rushabh2711 <shetarushabh@gmail.com>
2022-03-20 12:58:23 -0700

    send errors to frontend

--- backend/src/routes/image.js
+++ backend/src/routes/image.js
@@ -30,7 +30,7 @@ function uploadImage(key, body, contentType, res) {
 
   s3.upload(params, function (err, data) {
     if (err) {
-      res.status(400).send();
+      res.status(400).send(err);
     }
     console.log(`File uploaded successfully. ${data.Location}`);
     res.status(200).send(data.Location);
