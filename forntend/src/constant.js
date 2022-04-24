346170b65a8785aba4b6b3bee98d08c043ad3940
Rushabh2711 <shetarushabh@gmail.com>
2022-04-24 16:10:53 -0700

    ec2 url changed

--- forntend/src/constant.js
+++ forntend/src/constant.js
@@ -1,6 +1,6 @@
 class StringConstants {
-    url = "http://localhost:3001";
-    // url = "http://34.230.84.195:3001";
+    // url = "http://localhost:3001";
+    url = "http://34.230.84.195:3001";
     
   }
   
