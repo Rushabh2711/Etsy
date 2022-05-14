2e86ef7cafbaf0117a93c5807f422461d229cd01
Rushabh2711 <shetarushabh@gmail.com>
2022-05-14 11:32:14 -0700

    kafka request removed from order file - graphql

--- BACK_END/backend/src/routes/order.js
+++ BACK_END/backend/src/routes/order.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const Order = require('../service/Order');
-var kafka = require('../kafka/client');
+// var kafka = require('../kafka/client');
 const router = new express.Router();
 
 router.post('/createOrder', async (req, res) => {
-    // try {
-    //     await Order.createOrder(req.body.order);
-    //     res.status(200).send();
-    // } catch (e) {
-    //     console.log(e);
-    //     res.status(400).send(e);
-    // }
-    var data = await Order.createOrder(req.body.order);
-    if(req.body.order.products.length == 2) {
-        req.body.order.products[0].image = "https://etsy-bucket7.s3.us-east-2.amazonaws.com/9632587436922156.png";
-        req.body.order.products[1].image = "https://etsy-bucket7.s3.us-east-2.amazonaws.com/1329654987452213.png";
-        req.body.order._id = data._id
+    try {
+        await Order.createOrder(req.body.order);
+        res.status(200).send();
+    } catch (e) {
+        console.log(e);
+        res.status(400).send(e);
     }
-    kafka.make_request('createOrder',req.body.order, function(err,results){
-            console.log('in result');
-            console.log(results);
-            if (err){
-                console.log("Inside err");
-                res.status(400).send(err);
-            }else{
-                console.log("request successful");
-                res.status(200).send();
-                }
-        });
+    // var data = await Order.createOrder(req.body.order);
+    // if(req.body.order.products.length == 2) {
+    //     req.body.order.products[0].image = "https://etsy-bucket7.s3.us-east-2.amazonaws.com/9632587436922156.png";
+    //     req.body.order.products[1].image = "https://etsy-bucket7.s3.us-east-2.amazonaws.com/1329654987452213.png";
+    //     req.body.order._id = data._id
+    // }
+    // kafka.make_request('createOrder',req.body.order, function(err,results){
+    //         console.log('in result');
+    //         console.log(results);
+    //         if (err){
+    //             console.log("Inside err");
+    //             res.status(400).send(err);
+    //         }else{
+    //             console.log("request successful");
+    //             res.status(200).send();
+    //             }
+    //     });
 
 });
 
