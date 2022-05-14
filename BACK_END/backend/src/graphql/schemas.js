b9a59ed6f84e95a429770c1d34901b39ea1fce44
Rushabh2711 <shetarushabh@gmail.com>
2022-05-14 15:35:52 -0700

    schemas added in backend

--- BACK_END/backend/src/graphql/schemas.js
+++ BACK_END/backend/src/graphql/schemas.js
@@ -15,7 +15,6 @@ const UserType = new GraphQLObjectType({
     password: { type: GraphQLString },
     image: { type: GraphQLString },
     about: { type: GraphQLString },
-    // dob: {type: Date},
     address: { type: GraphQLString },
     country: { type: GraphQLString },
     gender: { type: GraphQLString },
@@ -24,6 +23,66 @@ const UserType = new GraphQLObjectType({
   }),
 });
 
+const OrderType = new GraphQLObjectType({
+  name: "Order",
+  fields: () => ({
+    user_id: { type: GraphQLString },
+    price:  { type: GraphQLString },
+    date:  { type: GraphQLString },
+    products: [{ type: GraphQLString }],
+  }),
+});
+
+const ProductType = new GraphQLObjectType({
+  name: "Product",
+  fields: () => ({
+    shop_id: { type: GraphQLString }, 
+    category: { type: GraphQLString }, 
+    count: { type: GraphQLFloat },
+    name: { type: GraphQLString }, 
+    image: { type: GraphQLString }, 
+    description:{ type: GraphQLString }, 
+    price: { type: GraphQLFloat },  
+    sell_count: { type: GraphQLFloat }
+  }),
+});
+
+const ShopType = new GraphQLObjectType({
+  name: "Shop",
+  fields: () => ({
+    user_id: { type: GraphQLString },
+    image: { type: GraphQLString },
+    name: { type: GraphQLString }
+  }),
+});
+
+const categoryType = new GraphQLObjectType({
+  name: "Category",
+  fields: () => ({
+    category_id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    user_id: { type: GraphQLString },
+  }),
+});
+
+const rootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getRestaurant: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parent, args) {
+        // const res = await RestaurantService.getRestaurantInfo(args.id);
+        return res.restaurant;
+      },
+    },
+  },
+});
+
 module.exports = {
   UserType,
+  OrderType,
+  ProductType,
+  ShopType,
+  categoryType
 };
