a93a2a20485e5e5f3c3e86fd335b3a0f2727a523
Rushabh2711 <shetarushabh@gmail.com>
2022-05-14 23:37:44 -0700

    mutation added

--- BACK_END/backend/src/graphql/mutations.js
+++ BACK_END/backend/src/graphql/mutations.js
@@ -1,9 +1,10 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLFloat } = graphql;
 const User = require('../service/User');
-const { UserType } = require("./schemas");
+const Product = require('../service/Product');
+const { UserType, ProductType } = require("./schemas");
 
-const mutation = new GraphQLObjectType({
+const addUSerMutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
     createUser: {
@@ -31,6 +32,37 @@ const mutation = new GraphQLObjectType({
   },
 });
 
+const addProductMutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addProduct: {
+      type: ProductType,
+      args: {
+        shop_id: { type: GraphQLString }, 
+        category: { type: GraphQLString }, 
+        count: { type: GraphQLFloat },
+        name: { type: GraphQLString }, 
+        image: { type: GraphQLString }, 
+        description:{ type: GraphQLString }, 
+        price: { type: GraphQLFloat },  
+        sell_count: { type: GraphQLFloat }
+      },
+      async resolve(parent, args) {
+        console.log(args);
+        try {
+          await Product.addProduct(
+            {...args}
+          );
+        } catch (error) {
+          console.log(error);
+        }
+        return { message: "Product Added Successfully",
+       };
+      },
+    },
+  },
+})
 
 
-module.exports = mutation;
+
+module.exports = addUSerMutation;
