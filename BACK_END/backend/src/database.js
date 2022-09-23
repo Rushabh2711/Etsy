077290aa116bbe5fe0bd8d3ec3486b66cf880ce4
Rushabh2711 <shetarushabh@gmail.com>
2022-09-22 23:20:02 -0700

    Update database.js
    remove key

--- BACK_END/backend/src/database.js
+++ BACK_END/backend/src/database.js
@@ -1,9 +1,9 @@
 // const mysql = require('mysql');
 
 // var conn = mysql.createPool({
-//     host: "etsy-db.cmoyamrdrxkl.us-east-1.rds.amazonaws.com",
-//     user: "admin",
-//     password: "2711rush1997",
+//     host: "",
+//     user: "",
+//     password: "",
 //     port: 3306,
 //     database: "etsy"
 // })
@@ -13,7 +13,7 @@
 // ------------------------mongo-----------------------
 const mongoose = require("mongoose");
 
-const url =  "mongodb+srv://rushabh:2711rush1997@cluster0.jks4l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const url =  "";
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
