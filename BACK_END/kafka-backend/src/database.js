690a10e5b591dbb577d3c9b0e53a36f37bf47678
Rushabh2711 <shetarushabh@gmail.com>
2022-09-22 23:18:54 -0700

    Update database.js
    remove key

--- BACK_END/kafka-backend/src/database.js
+++ BACK_END/kafka-backend/src/database.js
@@ -1,10 +1,10 @@
 // const mysql = require('mysql');
 
 // var conn = mysql.createPool({
-//     host: "etsy-db.cmoyamrdrxkl.us-east-1.rds.amazonaws.com",
-//     user: "admin",
-//     password: "2711rush1997",
-//     port: 3306,
+//     host: "",
+//     user: "",
+//     password: "",
+//     port: ,
 //     database: "etsy"
 // })
 
@@ -13,7 +13,7 @@
 // ------------------------mongo-----------------------
 const mongoose = require("mongoose");
 
-const url =  "mongodb+srv://rushabh:2711rush1997@cluster0.jks4l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const url =  "url";
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
